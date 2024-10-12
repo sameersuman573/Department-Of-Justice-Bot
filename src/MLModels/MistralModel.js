8307b0c442a2fb523e822a166c2761f10a2fe68e
sameersuman573 <sameer573cd@gmail.com>
2024-10-13 04:42:40 +0530

    Initialized Model

---
+++ src/MLModels/MistralModel.js
@@ -0,0 +1,12 @@
+
+import { ChatMistralAI } from "@langchain/mistralai";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const LLM = new ChatMistralAI({
+    model: "mistral-large-latest",
+    apiKey: process.env.MISTRAL_API_KEY,
+    temperature: 0.5,
+    maxRetries: 2,
+    maxTokens: 50,
+  }); 
