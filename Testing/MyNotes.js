5511c18561b917f7c7a5a1d3e5a5cd5eacfc8b7e
sameersuman573 <sameer573cd@gmail.com>
2024-10-13 04:02:23 +0530

    Removed UnNecessaryFiles

--- Testing/MyNotes.js
+++
@@ -1,9 +0,0 @@
-// 1. What is a LLM
-// An Llm is a AI model that is used to understand generate and respond to the human messages . These models are built on top of huge amount of data and are tarined upon them
-// it can understnad context and can perform relevant conversations 
-// It can do numerous amout of work from summarizing text to completing code and translatig languages
-
-
-
-
-// 2. What is prompt template 
--- src/KnowldegbaseLink/Gram_Nayalaya.js
+++
@@ -1,3 +0,0 @@
-
-export const Gram_nayalaya_URL = "https://dashboard.doj.gov.in/gn/operational_gram_nyayalaya"
-
