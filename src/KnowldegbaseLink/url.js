37bc568acae8d3e52850cc872a93bd3c94a8f449
sameersuman573 <sameer573cd@gmail.com>
2024-10-13 04:43:34 +0530

    Initialized Knowledge base

---
+++ src/KnowldegbaseLink/url.js
@@ -0,0 +1,28 @@
+
+// export const Gram_nayalaya_URL = "https://dashboard.doj.gov.in/gn/operational_gram_nyayalaya"
+
+
+export const urls = [
+    {
+      url: "https://www.tele-law.in/national-legal-services-authority.html",
+      description: "Information about National Legal Services Authority(NALSA)",
+    },
+    {
+      url: "https://dashboard.doj.gov.in/gn/fund_released",
+      description:
+        "Fund Released By Department Of justice for Gram Nyayalaya (Rs in Lakhs)",
+    },
+    {
+      url: "https://dashboard.doj.gov.in/gn/operational_gram_nyayalaya",
+      description:
+        "Number Of Operational Gram Nyayalaya in States",
+    },
+    {
+      url: "https://dashboard.doj.gov.in/gn/pendency_criminal",
+      description: "Description of the number of cases pending in Gram Nyayalaya",
+    },
+    {
+      url: "https://dashboard.doj.gov.in/gn/introduction",
+      description: "Description and features of About Gram Nyayalaya ",
+    },
+  ];
