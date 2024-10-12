37723ccd0a407bdef30fae4759bb8373fae9b585
sameersuman573 <sameer573cd@gmail.com>
2024-10-13 04:06:03 +0530

    Initialized Bot Chat prompt

---
+++ src/PromptTemplates/BotPrompt.js
@@ -0,0 +1,25 @@
+import {
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+  } from "@langchain/core/prompts";
+
+export const prompt = ChatPromptTemplate.fromMessages([
+    {
+      role: "system",
+      content: `
+      You are a helpuful assistant Who answer user queries
+      1. If the question is about Gram Nyayalaya, determine the most relevant URL from the list provided.
+      2. Use the corresponding loader tool to fetch the data.
+      3. For any other queries, use the general search tool.
+      `,
+    },
+    new MessagesPlaceholder("chat_history"),
+    {
+      role: "user",
+      content: "{input}",
+    },
+  
+    // ONE OF THE MOST IMPORTANT THING - IT IS USED TO MANAGE THE CONTEXT OF THE CONVERSATION
+    new MessagesPlaceholder("agent_scratchpad"),
+  ]);
+  
